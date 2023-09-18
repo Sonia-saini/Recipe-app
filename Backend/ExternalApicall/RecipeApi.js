2e66caf141f88e49f1156f7f7fdc4ccb84b9c51d
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-09-18 13:40:49 +0530

    checking

--- Backend/ExternalApicall/RecipeApi.js
+++ Backend/ExternalApicall/RecipeApi.js
@@ -1,5 +1,5 @@
 let getRecipe = async (query, page, limit,sort,sortDirection) => {
-  apikey = `337680e2a1354188922992e1606e5b4b`;
+  apikey = `ca284320ec884164935e9de2e0b7484d`;
   let recipe = await fetch(
     `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${apikey}&addRecipeInformation=true&addRecipeNutrition=true&instructionsRequired=true&offset=${
       page * limit
